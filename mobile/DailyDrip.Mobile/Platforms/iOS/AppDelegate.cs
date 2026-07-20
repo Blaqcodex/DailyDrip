dd5a0a3b146da515fbdae78d1161099a33620617
Blaqcodex <117004858+Blaqcodex@users.noreply.github.com>
2026-07-20 11:21:00 +0200

    build: initialize .NET MAUI project and configure required workloads

---
+++ mobile/DailyDrip.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DailyDrip.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
